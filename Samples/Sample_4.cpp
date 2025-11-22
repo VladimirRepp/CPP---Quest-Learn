710b116ee0709d740001579ad753b540460da0b5
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-11-22 20:50:44 +0700

    Add files via upload

--- Samples/Sample_4.cpp
+++ Samples/Sample_4.cpp
@@ -2,18 +2,12 @@
 #include <Windows.h>
 using namespace std;
 
-// Задание : Создай функцию расчета урона с шансом крита.
-
-// Задание : Создай функцию, проверяющую достаточно ли маны для заклинания.
-
-
-
 // Задание: Создай функцию для лечения героя.
 // heal - название функции, по названию просходит вызов функции 
 // int -  тип возвращаемого значение 
 // (...) - в скобках указываем что приходит в функцию, 
 // может ничего не указывать или указать какие то параметры (аргументы)
-int heal(int current_health, int potion_power, int max_health = 100) 
+int heal(int current_health, int potion_power, int max_health = 100)
 {
 	// current_health - текущее здоровье 
 	// potion_power - количестов здоровья лечения от зелья 
@@ -25,6 +19,27 @@ int heal(int current_health, int potion_power, int max_health = 100)
 		return max_health;
 	// else - не нужно 
 	return current_health;
+}
+
+// Задание: Создай функцию расчета урона с шансом крита.
+int calculate_damage(int base_damage, int crital_chance) {
+	int randValue = rand() % 100;
+
+	if (randValue < crital_chance) {
+		return base_damage * 2;
+	}
+
+	return base_damage;
+}
+
+// Задание: Создай функцию, проверяющую достаточно ли маны для заклинания.
+bool can_cast_spell(int current_mana, int spell_cost) {
+	if (current_mana >= spell_cost) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 int main()
@@ -50,4 +65,19 @@ int main()
 
 	cout << "Здоровье после полного лечения: " << health << endl;
 
+	int damage = calculate_damage(25, 25);
+	cout << "Нанесенный урон: " << damage << endl;
+
+	int current_mana = 20;
+	int fireball_cost = 25;
+
+	if (can_cast_spell(current_mana, fireball_cost)) {
+		cout << "Можем применить заклинание!" << endl;
+		current_mana -= fireball_cost; // current_mana = current_mana - fireball_cost;
+	}
+	else {
+		cout << "Недостаточно маны!" << endl;
+	}
+
+	cout << "Осталось маны: " << current_mana << endl;
 }
---
+++ Samples/Sample_5.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+using namespace std;
+
+// Задание: Создай инвентарь на 5 ячеек и добавь в него предметы.
+void BaseInventory_StaticArray() {
+	// Реализуем статичный массив 
+	// Тогда, когда зарнее знаем (до запуска программы) размер массива 
+	// То есть память под него выделяется  
+	// перед запуском программы 
+	string inventory[5];
+
+	inventory[0] = "Меч";
+	inventory[1] = "Щит";
+	inventory[2] = "Закленание - Фаир бол";
+	inventory[3] = "Закленание - Здоровье";
+	inventory[4] = "Закленание - молния";
+
+	cout << "Мой инвентарь:" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "[" << i << "] элемент: " << inventory[i] << endl;
+	}
+}
+
+void BaseInventory_DynamicArray() {
+	// Реализуем динамический массив 
+	// Тогда, когда заранее не знаем (до запуска) размер массива 
+	// То есть память под него выделяется 
+	// во время работы программы 
+
+	int countInventory = 0;
+
+	cout << "Укажите размер инвентаря: ";
+	cin >> countInventory;
+
+	string* inventory = new string[countInventory]; // выделение памяти
+
+	for (int i = 0; i < countInventory; i++) {
+		cout << "Введите [" << i << "] элемент: ";
+		cin >> inventory[i];
+	}
+
+	cout << "Мой инвентарь:" << endl;
+	for (int i = 0; i < countInventory; i++) {
+		cout << "[" << i << "] элемент: " << inventory[i] << endl;
+	}
+
+	delete[] inventory; // освободить память
+}
+
+void BaseInventory_Vector() {
+	// Реализуем через вектор 
+	// Тогда, когда заранее не знаем (до запуска) размер массива 
+	// То есть примерно как динамический массив 
+	// только работа с памятью происходит автоматически 
+
+	int countInventory = 0;
+
+	cout << "Укажите размер инвентаря: ";
+	cin >> countInventory;
+
+	vector<string> inventory(countInventory); // память выделаяется автоматически 
+
+	for (int i = 0; i < countInventory; i++) {
+		cout << "Введите [" << i << "] элемент: ";
+		cin >> inventory[i];
+	}
+
+	cout << "Мой инвентарь:" << endl;
+	for (int i = 0; i < countInventory; i++) {
+		cout << "[" << i << "] элемент: " << inventory[i] << endl;
+	}
+
+	// Память освобождается автоматически
+}
+
+// Задание: Реализуй добавление и удаление предметов из инвентаря.
+
+// Задание: Реализуй поиск конкретного предмета в инвентаре.
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	//BaseInventory_StaticArray();
+	//BaseInventory_DynamicArray();
+	BaseInventory_Vector();
+}
